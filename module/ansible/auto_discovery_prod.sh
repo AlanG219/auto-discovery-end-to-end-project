bcbd033b9f9d3cd86fa3f4d9d4cdd0c2fbc83e40
AlanG219 <alangallagher219@gmail.com>
2024-08-06 20:30:28 +0100

    pipeline running, error fixes commit

--- module/ansible/auto_discovery_prod.sh
+++ module/ansible/auto_discovery_prod.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 set -x
 
-# Define variables
-AWS_CLI_PATH='/usr/local/bin/aws'
-PROD_INVENTORY_FILE='/etc/ansible/prod-hosts'
-PROD_IPS_FILE='/etc/ansible/prod-ips.list'
-PROD_ASG_NAME='pet-auto_prod_asg'
-SSH_KEY_PATH="/home/ec2-user/.ssh/id_rsa" 
-WAIT_TIME=20
-
 # Function to discover AWS instances and save private IPs to file
 aws_discovery() {
     echo "Discovering AWS instances..."
-    $AWS_CLI_PATH ec2 describe-instances \
-        --filters "Name=tag:aws:autoscaling:groupName,Values=$PROD_ASG_NAME" \
+    /usr/local/bin/aws ec2 describe-instances \
+        --filters "Name=tag:aws:autoscaling:groupName,Values=pet-auto_prod_asg" \
         --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' \
-        --output text > "$PROD_IPS_FILE"
+        --output text > "/etc/ansible/prod-ips.list"
 }
 
 # Function to update Ansible inventory file
 update_inventory() {
     echo "Updating Ansible inventory file..."
-    echo "[webservers]" > "$PROD_INVENTORY_FILE"
+    echo "[webservers]" > "/etc/ansible/prod-hosts"
     while IFS= read -r instance; do
         ssh-keyscan -H "$instance" >> ~/.ssh/known_hosts
-        echo "$instance ansible_user=ec2-user ansible_ssh_private_key_file=$SSH_KEY_PATH" >> "$PROD_INVENTORY_FILE"
-    done < "$PROD_IPS_FILE"
+        echo "$instance ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/.ssh/id_rsa" >> "/etc/ansible/prod-hosts"
+    done < "/etc/ansible/prod-ips.list"
     echo "Inventory updated successfully."
 }
 
 # Function to wait for specified time
 wait_for_seconds() {
-    echo "Waiting for $WAIT_TIME seconds..."
-    sleep "$WAIT_TIME"
+    echo "Waiting for 20 seconds..."
+    sleep "20"
 }
 
 # Function to check and start Docker container if not running
@@ -40,13 +32,13 @@ check_docker_container() {
     echo "Checking and starting Docker containers if not running..."
     while IFS= read -r ip; do
         # Check if container is running
-        ssh -i "$SSH_KEY_PATH" ec2-user@"$ip" "docker ps --filter 'name=appContainer' --format '{{.Names}}'" | grep -q "appContainer"
+        ssh -i "/home/ec2-user/.ssh/id_rsa" ec2-user@"$ip" "docker ps --filter 'name=appContainer' --format '{{.Names}}'" | grep -q "appContainer"
         if [[ $? -ne 0 ]]; then
             # Container not running, execute script to start container
             echo "Starting Docker container on $ip..."
-            ssh -i "$SSH_KEY_PATH" ec2-user@"$ip" "/home/ec2-user/scripts/script.sh"
+            ssh -i "/home/ec2-user/.ssh/id_rsa" ec2-user@"$ip" "/home/ec2-user/scripts/script.sh"
         fi
-    done < "$PROD_IPS_FILE"
+    done < "/etc/ansible/prod-ips.list"
 }
 
 # Main function block
--- module/ansible/auto_discovery_stage.sh
+++ module/ansible/auto_discovery_stage.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 set -x
 
-# Define variables
-AWS_CLI_PATH='/usr/local/bin/aws'
-STAGE_INVENTORY_FILE='/etc/ansible/stage-hosts'
-STAGE_IPS_FILE='/etc/ansible/stage-ips.list'
-STAGE_ASG_NAME='pet-auto_stage_asg'
-SSH_KEY_PATH="/home/ec2-user/.ssh/id_rsa" 
-WAIT_TIME=20
-
 # Function to discover AWS instances and save private IPs to file
 aws_discovery() {
     echo "Discovering AWS instances..."
-    $AWS_CLI_PATH ec2 describe-instances \
-        --filters "Name=tag:aws:autoscaling:groupName,Values=$STAGE_ASG_NAME" \
+    /usr/local/bin/aws ec2 describe-instances \
+        --filters "Name=tag:aws:autoscaling:groupName,Values=pet-auto_stage_asg" \
         --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' \
-        --output text > "$STAGE_IPS_FILE"
+        --output text > "/etc/ansible/stage-ips.list"
 }
 
 # Function to update Ansible inventory file
 update_inventory() {
     echo "Updating Ansible inventory file..."
-    echo "[webservers]" > "$STAGE_INVENTORY_FILE"
+    echo "[webservers]" > "/etc/ansible/stage-hosts"
     while IFS= read -r instance; do
         ssh-keyscan -H "$instance" >> ~/.ssh/known_hosts
-        echo "$instance ansible_user=ec2-user ansible_ssh_private_key_file=$SSH_KEY_PATH" >> "$STAGE_INVENTORY_FILE"
-    done < "$STAGE_IPS_FILE"
+        echo "$instance ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/.ssh/id_rsa" >> "/etc/ansible/stage-hosts"
+    done < "/etc/ansible/stage-ips.list"
     echo "Inventory updated successfully."
 }
 
 # Function to wait for specified time
 wait_for_seconds() {
-    echo "Waiting for $WAIT_TIME seconds..."
-    sleep "$WAIT_TIME"
+    echo "Waiting for 20 seconds..."
+    sleep "20"
 }
 
 # Function to check and start Docker container if not running
@@ -40,13 +32,13 @@ check_docker_container() {
     echo "Checking and starting Docker containers if not running..."
     while IFS= read -r ip; do
         # Check if container is running
-        ssh -i "$SSH_KEY_PATH" ec2-user@"$ip" "docker ps --filter 'name=appContainer' --format '{{.Names}}'" | grep -q "appContainer"
+        ssh -i "/home/ec2-user/.ssh/id_rsa" ec2-user@"$ip" "docker ps --filter 'name=appContainer' --format '{{.Names}}'" | grep -q "appContainer"
         if [[ $? -ne 0 ]]; then
             # Container not running, execute script to start container
             echo "Starting Docker container on $ip..."
-            ssh -i "$SSH_KEY_PATH" ec2-user@"$ip" "/home/ec2-user/scripts/script.sh"
+            ssh -i "/home/ec2-user/.ssh/id_rsa" ec2-user@"$ip" "/home/ec2-user/scripts/script.sh"
         fi
-    done < "$STAGE_IPS_FILE"
+    done < "/etc/ansible/stage-ips.list"
 }
 
 # Main function block
